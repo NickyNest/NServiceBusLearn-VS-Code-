ee2fc5b10c6c1b8eb2e8ea8461a1cbb54870255e
NickyNest <nesterenko.nicky@gmail.com>
2019-05-10 11:17:05 +0300

    added handling of an exception

--- Sales/PlaceOrderHandler.cs
+++ Sales/PlaceOrderHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Messages;
 using NServiceBus;
@@ -8,10 +9,16 @@ namespace Sales
     public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
     {
         static ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+        static Random random = new Random();
 
         public Task Handle(PlaceOrder message, IMessageHandlerContext context)
         {
             log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+
+            if(random.Next(0, 5) == 0)
+            {
+                throw new Exception("Boom");
+            }
 
             var orderPlaced = new OrderPlaced
             {
--- Sales/Program.cs
+++ Sales/Program.cs
@@ -13,6 +13,14 @@ namespace Sales
             var endpointConfiguration = new EndpointConfiguration("Sales");
             var transport = endpointConfiguration.UseTransport<LearningTransport>();
 
+            var recoverability = endpointConfiguration.Recoverability();
+            recoverability.Immediate(immediate => { immediate.NumberOfRetries(2); });
+            recoverability.Delayed(delayed => 
+            {
+                delayed.NumberOfRetries(1);
+                delayed.TimeIncrease(TimeSpan.FromSeconds(5));
+            });
+
             var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
 
             Console.WriteLine("Press Enter to exit.");
